85a3b1c4c25a3276a219549ef30646d941cc4532
Artem2313 <huselnykov@gmail.com>
2020-01-13 12:36:57 +0200

    Updates

---
+++ precache-manifest.08ead21c0df12553d1994fcd239281ed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad7ba8f5da7bc451f53cdcdaabb8800f",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "c7055e2bfc1d9a77f83f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.10f7a28f.chunk.css"
+  },
+  {
+    "revision": "a183a401567f4be8957b",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.42ba3bd7.chunk.js"
+  },
+  {
+    "revision": "c7055e2bfc1d9a77f83f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c470aaf1.chunk.js"
+  },
+  {
+    "revision": "191fa215192447a8301c",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.3b37d30b.js"
+  }
+]);
