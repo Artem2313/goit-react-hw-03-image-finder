f4b22d03d11794dc293024ff41ecfe146ba257f6
Artem2313 <huselnykov@gmail.com>
2020-01-31 12:14:06 +0200

    deleted scroll down

--- src/components/Buttons/ClickMoreButton.jsx
+++ src/components/Buttons/ClickMoreButton.jsx
@@ -3,10 +3,6 @@ import PropTypes from 'prop-types';
 import styles from './Button.module.css';
 
 const ClickMoreButton = ({ clickMore }) => {
-  window.scrollTo({
-    top: document.documentElement.scrollHeight,
-    behavior: 'smooth',
-  });
   return (
     <button onClick={clickMore} className={styles.Button} type="button">
       More
